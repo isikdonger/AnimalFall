c4df796557f8263f0d6c57c636cf83430e4a5fcc
Işık Dönger <isikdonger04@gmail.com>
2025-04-07 20:09:29 +0300

    Ishigo Games

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "961286195541"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "961286195541-9o4mih4r5d6nbcktkfm99mqikeitde8e.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "961286195541-fdddf4f13qc5uq1ipdbrctuo5efs3ak8.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
